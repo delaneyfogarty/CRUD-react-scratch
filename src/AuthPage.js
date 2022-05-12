86df3b22f351e5b44c6483efa75b6a8286d1e506
Delaney Fogarty <delaney.fk@gmail.com>
2022-05-12 13:45:53 -0700

    cats list page

--- src/AuthPage.js
+++ src/AuthPage.js
@@ -1,5 +1,82 @@
-import React from 'react';
+import { useState } from 'react';
+import { signIn, signUp, getUser } from './services/fetch-utils.js';
 
-export default function AuthPage() {
-  return <div>AuthPage</div>;
+export default function AuthPage({ setEmail, setToken }) {
+  const [signUpEmail, setSignUpEmail] = useState('');
+  const [signUpPassword, setSignUpPassword] = useState('');
+  const [{ email: signInEmail, password: signInPassword }, setSignInFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  async function handleSignIn(e) {
+    e.preventDefault();
+    await signIn(signInEmail, signInPassword);
+    const {
+      access_token,
+      user: { email },
+    } = getUser();
+
+    setEmail(email);
+    setToken(access_token);
+  }
+
+  async function handleSignUp(e) {
+    e.preventDefault();
+
+    await signUp(signUpEmail, signUpPassword);
+    const {
+      access_token,
+      user: { email },
+    } = getUser();
+
+    setEmail(email);
+    setToken(access_token);
+  }
+
+  return (
+    <div className="auth">
+      <h1>
+        <em>Cats!</em>
+      </h1>
+      <form onSubmit={handleSignIn}>
+        <label>
+          Email
+          <input
+            required
+            type="email"
+            name="email"
+            value={signInEmail}
+            onChange={(e) => setSignInFormData({ email: e.target.value, password: signInPassword })}
+          />
+        </label>
+        <label>
+          Password
+          <input
+            required
+            type="password"
+            name="password"
+            value={signInPassword}
+            onChange={(e) => setSignInFormData({ email: signInEmail, password: e.target.value })}
+          />
+        </label>
+        <button>Sign In</button>
+      </form>
+      <form onSubmit={handleSignUp}>
+        <label>
+          Email
+          <input value={signUpEmail} onChange={(e) => setSignUpEmail(e.target.value)} />
+        </label>
+        <label>
+          Password
+          <input
+            type="password"
+            value={signUpPassword}
+            onChange={(e) => setSignUpPassword(e.target.value)}
+          />
+        </label>
+        <button>Sign Up</button>
+      </form>
+    </div>
+  );
 }
--- src/ListPage.js
+++ src/ListPage.js
@@ -1,5 +1,24 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { getCats } from './services/fetch-utils';
+import Cat from './Cat';
 
 export default function ListPage() {
-  return <div>ListPage</div>;
+  const [cats, setCats] = useState([]);
+
+  useEffect(() => {
+    async function loadCats() {
+      const catData = await getCats();
+      setCats(catData);
+    }
+
+    loadCats();
+  }, []);
+
+  return (
+    <div>
+      {cats.map((cat, i) => (
+        <Cat key={cat.name + i} cat={cat} />
+      ))}
+    </div>
+  );
 }
