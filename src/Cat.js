f8b076b393e5ac6a0636301074792c0569c7924d
Delaney Fogarty <delaney.fk@gmail.com>
2022-05-12 13:53:03 -0700

    cat component

--- src/Cat.js
+++ src/Cat.js
@@ -1,5 +1,19 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function Cat() {
-  return <div>Cat</div>;
+export default function Cat({ cat }) {
+  return (
+    <Link to={`/cats/${cat.id}`}>
+      <div className="cat">
+        <h3>{cat.name}</h3>
+        <p>
+          {' '}
+          A {cat.type} that loves to eat {cat.fav_food}{' '}
+        </p>
+        <p>
+          {' '}
+          I am {cat.age} years old and a {cat.zodiac}{' '}
+        </p>
+      </div>
+    </Link>
+  );
 }
